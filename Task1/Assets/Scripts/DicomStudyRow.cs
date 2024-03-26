4abf8057c91d5dae08a9f993213999968c89c051
hsskia <hskim@skia.kr>
2024-03-26 16:49:27 +0900

    field 들의 접근 지정자 변경
    public 을 private 으로 변경 및 if 문 nested 해소

--- Task1/Assets/Scripts/DicomStudyRow.cs
+++ Task1/Assets/Scripts/DicomStudyRow.cs
@@ -4,14 +4,16 @@ using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using Mars.db;
 using Unity.VisualScripting;
-namespace StudyRow{
-    public class DicomStudyRow : MonoBehaviour{
+namespace StudyRow
+{
+    public class DicomStudyRow : MonoBehaviour
+    {
 
-        public Text idText;
-        public Text nameText;
-        public Text descriptionText;
-        public Text patientIdText;
-        public Text numSeriesText;
+        [SerializeField] private Text idText;
+        [SerializeField] private Text nameText;
+        [SerializeField] private Text descriptionText;
+        [SerializeField] private Text patientIdText;
+        [SerializeField] private Text numSeriesText;
 
         public void SetStudyData(DicomStudy study)
         {
--- Task1/Assets/Scripts/DicomTableview.cs
+++ Task1/Assets/Scripts/DicomTableview.cs
@@ -15,29 +15,35 @@ using StudyRow;
 
 public class DicomTableview : MonoBehaviour
 {
-    public ScrollRect studyScrollview;
-    public ScrollRect seriesScrollview;
 
-    public GameObject scrollviewContent;
-    public GameObject rowContent;
-    public GameObject seriesContent;
+    [SerializeField] private ScrollRect studyScrollview;
 
-    public Text seriesText;
+    [SerializeField] private ScrollRect seriesScrollview;
 
-    string dicomURL = "http://10.10.20.173:5080/v2/Dicom/";
-    public string studyId;
+    [SerializeField] private GameObject scrollviewContent;
 
-    public void OnClick()
+    [SerializeField] private GameObject rowContent;
+
+    [SerializeField] private GameObject seriesContent;
+
+    [SerializeField] private Text seriesText;
+
+    private const string dicomURL = "http://10.10.20.173:5080/v2/Dicom/";
+    private string studyId;
+
+    void OnClick()
     {
         GameObject btn = EventSystem.current.currentSelectedGameObject;
         studyId = btn.GetComponentInChildren<Text>().text;
 
         // ID 버튼을 클릭했을 때 처음으로 원상복구 되도록
-        if (studyId == "ID"){ 
+        if (studyId == "ID")
+        {
             SetStudyVisibility(true);
             RemoveSeriesObject();
         }
-        else{
+        else
+        {
             RemoveSeriesObject();
             StartCoroutine(GetSeriesData());
             SetStudyVisibility(false);
@@ -45,80 +51,89 @@ public class DicomTableview : MonoBehaviour
     }
 
     // 스크롤뷰 초기화 -> 데이터의 크기만큼 화면이 출력되도록
-    void ResetScrollview(){
+    void ResetScrollview()
+    {
         studyScrollview.gameObject.SetActive(false);
         studyScrollview.gameObject.SetActive(true);
     }
 
-    void RemoveSeriesObject(){
+    void RemoveSeriesObject()
+    {
         Transform[] childObject = seriesContent.GetComponentsInChildren<Transform>(true);
-        for (int i = 1; i < childObject.Length; i++){
-
-            // 원본 제외 복사한 항목 모두 삭제
-            if(childObject[i].name.Contains("Clone")){
+        for (int i = 1; i < childObject.Length; i++)
+        {
+            // Study data 만 화면에 출력하기 위해 Series data 를 의미하는 Clone 된 Object 들을 제거
+            if (childObject[i].name.Contains("Clone"))
+            {
                 Destroy(childObject[i].gameObject);
             }
         }
     }
 
-    void SetStudyVisibility(bool check){
-        if(check == true){
+    void SetStudyVisibility(bool check)
+    {
+        if (check)
+        {
             // study data가 활성화되면 series 데이터는 비활성화되어 화면이 겹치는 것 방지
-            seriesScrollview.gameObject.SetActive(false);             
+            seriesScrollview.gameObject.SetActive(false);
         }
-        else{     
+        else
+        {
             seriesScrollview.gameObject.SetActive(true);
             seriesText.gameObject.SetActive(true);
-        }   
-            
+        }
+
         Transform[] childObject = scrollviewContent.GetComponentsInChildren<Transform>(true);
 
         /* index 0번은 모든 Dicom Study Data Row 를 포함하는 GameObject 이기 때문에 이를 비활성화 시킬 경우
            Series Data 와 일치하는 Study Data 는 남겨두고자 하는 원래 목적이 사라질 수 있기 때문에
-           index 를 1번 부터 시작하여 Series Data 가 표현하는 studyID 와 일치하는 Study Data Row 는
+           index 를 1번 부터 시작하여 Series Data 가 표현하는 studyId 와 일치하는 Study Data Row 는
            활성화된 상태로 남겨둠.
         */
-        for (int i = 1; i < childObject.Length; i++){
-            if(check == true){
-                childObject[i].gameObject.SetActive(true);
+        for (int i = 1; i < childObject.Length; i++)
+        {
+            childObject[i].gameObject.SetActive(true);
+            if (check) continue;
+
+            string childName = childObject[i].name;
+            string childId = Regex.Replace(childName, @"[^0-9]", "");
+
+            if (childName.Contains("Clone") & // 복제된 이미지가 study data 를 나타내기 때문에 Clone 을 포함한 데이터에 접근
+            (childId != studyId)) // studyId 와 일치하지 않은 study data 를 가진 Object 는 비활성화
+            {
+                childObject[i].gameObject.SetActive(false);
             }
-            else{
-                string childName = childObject[i].name;
-                string childId = Regex.Replace(childName, @"[^0-9]", "");
-                if (childName.Contains("Clone") & 
-                (childId != studyId)) // id가 일치하는 data 외에는 모두 비활성화
-                {
-                    childObject[i].gameObject.SetActive(false);
-                }
-            }
+
         }
 
-        ResetScrollview();        
+        ResetScrollview();
     }
 
     void AddDicomSeriesRows(JArray dicomSeries)
     {
         seriesContent.SetActive(true);
 
-        // study id에 일치하는 series 데이터들
-        foreach (JObject item in dicomSeries) {
+        foreach (JObject item in dicomSeries)
+        {
             DicomSeries series = item.ToObject<DicomSeries>();
             string seriesValue = "-------------------------------------------------------------------------------------------- \n";
-            
-            // series 데이터 출력
-            foreach (var property in typeof(DicomSeries).GetProperties()){
+
+            foreach (var property in typeof(DicomSeries).GetProperties())
+            {
                 object val = property.GetValue(series);
                 seriesValue += $"{property.Name}: {val} \n";
             }
             Text seriesData = (Text)Instantiate(seriesText, seriesContent.transform);
             seriesData.text = seriesValue;
-            
+
         }
         seriesText.gameObject.SetActive(false);
     }
 
-    void AddDicomStudyRows(JArray dicomStudy){
-        foreach (JObject item in dicomStudy) {
+    void AddDicomStudyRows(JArray dicomStudy)
+    {
+        foreach (JObject item in dicomStudy)
+        {
             GameObject newRow = Instantiate(rowContent, scrollviewContent.transform);
             DicomStudyRow dicomStudyRow = newRow.GetComponent<DicomStudyRow>();
             DicomStudy study = item.ToObject<DicomStudy>();
@@ -128,7 +143,8 @@ public class DicomTableview : MonoBehaviour
         ResetScrollview();
     }
 
-    void Start(){
+    void Start()
+    {
         StartCoroutine(GetStudyData());
     }
 
@@ -145,7 +161,7 @@ public class DicomTableview : MonoBehaviour
         {
             JArray dicomStudy = JArray.Parse(reqStudy.downloadHandler.text);
             AddDicomStudyRows(dicomStudy);
-            
+
         }
     }
 
